97658fe2dd5b2df706e8cc95095f357139b5ef1a
AlicjaLukaszewicz <alisa.lukaszewicz@gmail.com>
2023-11-27 08:24:18 +0100

    Refactor Canvas class to handle different drawing tools.

--- simple-ms-paint/Canvas/Canvas.cpp
+++ simple-ms-paint/Canvas/Canvas.cpp
@@ -1,8 +1,11 @@
 #include "Canvas.h"
 
-Canvas::Canvas(float width, float height, Vector2f position, float padding) {
-    canvas.setSize(Vector2f(width - 2 * padding, height - 2 * padding));
-    canvas.setPosition(position.x + padding, position.y + padding);
+Canvas::Canvas(float width, float height, Vector2f position, float padding)
+    : canvasPosition(position.x + padding, position.y + padding), pixelSize(5.0f)  {
+    canvasSize = { width - 2 * padding, height - 2 * padding };
+
+    canvas.setSize(canvasSize);
+    canvas.setPosition(canvasPosition);
     canvas.setFillColor(Color::White);
 
     pixels.setPrimitiveType(Points);
@@ -15,36 +18,54 @@ void Canvas::drawTo(RenderWindow& window) {
 }
 
 void Canvas::onMouseHold(Vector2f mousePosition, Button enabledButton) {
-    if (enabledButton.getFunction() == ButtonFunction::pencil) {
-        if (Mouse::isButtonPressed(Mouse::Left)) {
-            if (mousePosition.x >= canvas.getPosition().x &&
-                mousePosition.y >= canvas.getPosition().y &&
-                mousePosition.x <= canvas.getPosition().x + canvas.getSize().x &&
-                mousePosition.y <= canvas.getPosition().y + canvas.getSize().y) {
+    if (isMouseOnCanvas(mousePosition)) {
+        if (enabledButton.getFunction() == ButtonFunction::pencil) {
+            handlePencilAction(mousePosition);
+        }
+        else if (enabledButton.getFunction() == ButtonFunction::eraser) {
+            handleEraserAction(mousePosition);
+        }
+    }
+}
 
-                float pixelSize = 5.0f; // Adjust this size based on your preference
-                std::size_t index = 0;
+void Canvas::handlePencilAction(Vector2f mousePosition) {
+    updateCanvasPixels(mousePosition, Color::Black);
+}
 
-                // Draw additional points around the central position
-                for (float i = -pixelSize / 2; i <= pixelSize / 2; ++i) {
-                    for (float j = -pixelSize / 2; j <= pixelSize / 2; ++j) {
-                        // Check if the new position is within the canvas boundaries
-                        if (mousePosition.x + i >= canvas.getPosition().x &&
-                            mousePosition.y + j >= canvas.getPosition().y &&
-                            mousePosition.x + i <= canvas.getPosition().x + canvas.getSize().x &&
-                            mousePosition.y + j <= canvas.getPosition().y + canvas.getSize().y) {
+void Canvas::handleEraserAction(Vector2f mousePosition) {
+    updateCanvasPixels(mousePosition, canvas.getFillColor());
+}
 
-                            // Calculate the index in the VertexArray based on the modified mouse position
-                            index = static_cast<std::size_t>((mousePosition.x + i - canvas.getPosition().x) +
-                                (mousePosition.y + j - canvas.getPosition().y) * canvas.getSize().x);
+void Canvas::updateCanvasPixels(Vector2f mousePosition, const Color& newColor) {
+    std::size_t index = 0;
 
-                            // Update the color of the corresponding vertex in the VertexArray to black
-                            pixels[index].position = mousePosition + Vector2f(i, j);
-                            pixels[index].color = Color::Black;
-                        }
-                    }
-                }
+    for (float i = -pixelSize / 2; i <= pixelSize / 2; ++i) {
+        for (float j = -pixelSize / 2; j <= pixelSize / 2; ++j) {
+            if (isModifiedPositionOnCanvas(mousePosition, i, j)) {
+                index = calculateIndex(mousePosition, i, j);
+
+                pixels[index].position = mousePosition + Vector2f(i, j);
+                pixels[index].color = newColor;
             }
         }
     }
+}
+
+bool Canvas::isMouseOnCanvas(Vector2f mousePosition) const {
+    return (mousePosition.x >= canvas.getPosition().x &&
+        mousePosition.y >= canvas.getPosition().y &&
+        mousePosition.x <= canvas.getPosition().x + canvas.getSize().x &&
+        mousePosition.y <= canvas.getPosition().y + canvas.getSize().y);
+}
+
+bool Canvas::isModifiedPositionOnCanvas(Vector2f mousePosition, float i, float j) const {
+    return (mousePosition.x + i >= canvas.getPosition().x &&
+        mousePosition.y + j >= canvas.getPosition().y &&
+        mousePosition.x + i <= canvas.getPosition().x + canvas.getSize().x &&
+        mousePosition.y + j <= canvas.getPosition().y + canvas.getSize().y);
+}
+
+size_t Canvas::calculateIndex(Vector2f mousePosition, float i, float j) const {
+    return static_cast<size_t>((mousePosition.x + i - canvas.getPosition().x) +
+        (mousePosition.y + j - canvas.getPosition().y) * canvas.getSize().x);
 }
--- simple-ms-paint/Canvas/Canvas.h
+++ simple-ms-paint/Canvas/Canvas.h
@@ -9,11 +9,22 @@ private:
 	RectangleShape canvas;
 	VertexArray pixels;
 
+	Vector2f canvasSize;
+	Vector2f canvasPosition;
+	float pixelSize;
+
+	bool isMouseOnCanvas(Vector2f mousePosition) const;
+	void handlePencilAction(Vector2f mousePosition);
+	void handleEraserAction(Vector2f mousePosition);
+	void updateCanvasPixels(Vector2f mousePosition, const Color& newColor);
+	size_t calculateIndex(Vector2f mousePosition, float i, float j) const;
+	bool isModifiedPositionOnCanvas(Vector2f mousePosition, float i, float j) const;
+
 public:
 	Canvas(float width, float height, Vector2f position, float padding);
-	void draw(RectangleShape pixel);
-	void drawTo(RenderWindow& window);
 
+	void drawTo(RenderWindow& window);
 	void onMouseHold(Vector2f mousePosition, Button enabledButton);
+	void onMouseClick(Vector2f mousePosition, Button enabledButton);
 };
 
